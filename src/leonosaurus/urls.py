7eaf7933471383b07d8e1e76035c427432fca7e5
THEBAULT Julien <julien.thebault@numberly.com>
2024-02-12 21:49:57 +0100

    feat(authentication): initial gitlab oauth support
    closes #1

--- src/leonosaurus/urls.py
+++ src/leonosaurus/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from deploydocus.views import AboutView
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
+    path("about/", AboutView.as_view()),
 ]
